7931d90352be85395ce4f8d225d9ded06b530837
tkote <7146253+tkote@users.noreply.github.com>
2022-02-18 23:59:07 +0900

    updated

--- twitter-filtered-stream/pyspark/tweet_tokenizer.py
+++ twitter-filtered-stream/pyspark/tweet_tokenizer.py
@@ -12,7 +12,7 @@ def tokenize(s: str) -> List[str]:
         surface = token.surface
         part = token.part_of_speech
         baseform = token.base_form
-        #print(f'surface={surface}, type={part}, baseform={baseform}')        
+        #print(f'surface={surface}, part={part}, baseform={baseform}')        
         
         if(len(surface) > 32):
             # skip too long keyword 
@@ -27,7 +27,7 @@ def tokenize(s: str) -> List[str]:
             #stop words
             #print(f'[{surface}] dropped (stop words)')
             pass
-        elif(part.startswith('名詞')):
+        elif(part.startswith('名詞') and not part.startswith('名詞,非自立')):
             if(re.match(r'^(こと|もの|www|http[s]?|[0-9]+)$', surface)):
                 # not much meaningful
                 #print(f'[{surface}] dropped (not much meaningful)')
@@ -35,7 +35,7 @@ def tokenize(s: str) -> List[str]:
             else:
                 #print(f'[{surface}] added')
                 words.append(surface)
-        elif(part.startswith('動詞') or part.startswith('形容詞')):
+        elif(part.startswith('動詞,自立') or part.startswith('形容詞,自立')):
             if(re.match(r'^(する|なる|言う|見る|思う|聞く|いう|やる|行く|いく|できる|出来る|ある|ない|いる|いない|http[s]?|[0-9]+)$', baseform)):
                 # not much meaningful
                 #print(f'[{baseform}] dropped (not much meaningful)')
@@ -43,5 +43,16 @@ def tokenize(s: str) -> List[str]:
             else:
                 #print(f'[{baseform}] added')
                 words.append(baseform)
+        else:
+            #print(f'[{baseform}] unknown !!!')
+            pass
+
     print(words)
     return words
+
+if __name__ == '__main__':
+    import sys
+    with open(sys.argv[1]) as f:
+        for line in f:
+            print('----------')
+            tokenize(line)
